5a3a33fc2fe57480d6c8d6f4737d34d388a094f0
jetsetx <breakxcore@gmail.com>
2017-11-29 22:30:46 -0500

    Stream 11/29/2017

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const deadline = 15
+var events = [];
+//EVENTS will have 3 properties: Name, Date/Deadline, (optional) link
 
-client.login('MzEwNjAzNDI3Mzg0MjYyNjU3.C_Ad_w.9Zr3jYsMMUZTk2y_azxfUVBw_jk');
+client.login('xxxxxxxxxxxxxxxxx');
 
 
 client.on('ready', () => {
@@ -14,20 +17,63 @@ client.on('message', message => {
   }
 });
 
-client.on('guildMemberAdd', member => {
-  member.guild.defaultChannel.send('Welcome to the Guild! Be sure to read our rules in #welcome, and enjoy your stay!');
-});
+//client.on('guildMemberAdd', member => {
+//  member.guild.defaultChannel.send('Welcome to the Order! Be sure to read our rules in #welcome, and enjoy your stay!');
+//});
 
+//OSS needs to 1 - tell what the date is
+//3- Tell the user time until signup date.
 client.on('message', message => {
-  if (message.content != '!oss') {
-    return;
+  var cmd = '!oss';
+  if (message.content.startsWith(cmd)) {
+    var now = new Date();
+    var current = now.getDate();
+    if (current < deadline) {
+      message.reply('You have ' + deadline - current + ' days left to sign up!');
+      message.reply('Here\'s the link: For now put a link here cuz u suck')
+    } else if (current == deadline) {
+      message.reply('Today\'s the last day to sign up! Hurry: put a link here dummy')
+    } else {
+      message.reply('You are unfortunately past the deadline: follow https://twitch.tv/ogmtv to find out when we next go live! See you next month!')
+    }
   }
-  message.reply('One Shots & Shots Signup - May: https://goo.gl/forms/bsqcgTm2zwVH69Ar1')
 });
 
+//Admin-only Add events
+clent.on('message,'
+  message => {
+    var cmd = '!addevent'
+    if (message.content.startsWith(cmd)) {
+      if (message.member.hasPermission("ADMINISTRATOR"){
+          //add event
+          message.reply('great job judy! NOW ADD THE EVENT DUMMY')
+        } else {
+          message.reply('This is an Admin-only action. Sorry!')
+        })
+    }
+  });
+//Admin-only remove events
+//Can events expire?
+
+//list of upcoming events
+
+
+//what roles are in this server?
 client.on('message', message => {
-  var tags = message.roles();
-  if(message.content === '!tagme'){
-  GuildMember.addRole(tags);
-  message.reply('You have been added to the following roles:' + tags)
-}});
+  var cmd = "!roles"
+  if (message.content.startsWith(cmd)) {
+    var roletext = message.guild.roles();
+    message.reply('Here are applicable roles: ' + roletext)
+  }
+});
+
+//give client a role
+client.on('message', message => {
+  var cmd = "!roleme"
+  if (message.content.startsWith(cmd)) {
+    var rolenick = message.content.slice(cmd.length + 1);
+    var roleobj = message.guild.roles.find("name", rolenick);
+    var result = message.member.addRole(roleobj);
+    result.then(() => message.reply(message.member.nickname + ', you have been added to the following roles: ' + roleme), () => message.reply('I am sorry, ' + message.member.nickname + ', I believe I cannot do that. Try again!'));
+  }
+});
